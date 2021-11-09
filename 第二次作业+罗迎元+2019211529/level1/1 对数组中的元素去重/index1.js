5c678481350f1ccedb1c9106ed9dc813454f90bb
LYY <luoyingyuan06@qq.com>
2021-11-09 10:36:15 +0800

    完成level1第一个

---
+++ 第二次作业+罗迎元+2019211529/level1/1 对数组中的元素去重/index1.js
@@ -0,0 +1,25 @@
+var arr=[1,2,3,3,4,'a','a','b','c'];
+
+function reduce(arr){
+    var res=[];
+
+    for(var i=0; i<arr.length; i++){
+        if(i == 0){
+            res[0] = arr[0];
+        }
+        else{
+            for(var j=0; j<res.length; j++){
+                if(res[j] === arr[i]){
+                    break;
+                }
+                else if(j == res.length - 1){
+                    res[res.length] = arr[i];
+                }
+            }
+        }
+    }
+
+    return res;
+}
+
+console.log(reduce(arr));  //[1, 2, 3, 4, "a", "b", "c"]
