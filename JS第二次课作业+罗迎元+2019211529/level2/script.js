dfdd347dbe22aefc3e168b0eb0f9a2365c138581
LYY <luoyingyuan06@qq.com>
2021-11-20 20:37:18 +0800

    level_2完成

--- JS第二次课作业+罗迎元+2019211529/level2/script.js
+++ JS第二次课作业+罗迎元+2019211529/level2/script.js
@@ -6,17 +6,20 @@ function course()
 
 function student(name,age,gender)
     {
+        course.call(this)
         this.name=name;
         this.age=age;
         this.gender=gender;
+        var that = this;
         this.moreinfo=function(){
             var	detail=function(){
-                console.log(this.gender);
+                console.log(that.gender);
             }
             detail();
+            // console.log(this.gender);
         }
     }
-
+    
     var student1 = new student("小明",18,"男");
     console.log(student1.coursename,student1.semester); //课程名字和第几期
     student1.moreinfo();//你的性别
