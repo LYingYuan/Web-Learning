4027727f3d0d3657aebab1907cfc0e9638e7ddc0
LYY <luoyingyuan06@qq.com>
2021-11-27 23:07:27 +0800

    Level3完成

--- JS第三次课作业+罗迎元+2019211529/level3/script.js
+++ JS第三次课作业+罗迎元+2019211529/level3/script.js
@@ -1,1 +1,23 @@
-console.log('hello')
+var text = document.querySelector('.text');
+var btn1 = document.querySelector('.btn1');
+var output = document.querySelector('.output');
+
+btn1.onclick = function() {
+    if (text.value == ''){
+        alert('您没有输入内容,请重新输入！');
+        return false;
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = '<div class="text2">' + text.value + '<button class="btn2">删除</button>' + '</div>';
+        output.insertBefore(li, output.childNodes[0])
+        text.value = '';
+    }
+    var dels = document.querySelectorAll('.btn2');
+    let len = dels.length;
+    for(var i = 0 ; i < len ; i++){
+        dels[i].onclick = function() {
+            output.removeChild(this.parentNode.parentNode)
+        }
+    }
+}
