8fc8aef70afe7c33884f766ba9f6875f90b2dbce
LYY <luoyingyuan06@qq.com>
2021-12-12 17:10:56 +0800

    Level4完成

--- Ajax+ES6+罗迎元+2019211529/Level4/script.js
+++ Ajax+ES6+罗迎元+2019211529/Level4/script.js
@@ -1,42 +1,42 @@
 const url = "http://cloud-music.pl-fe.cn/personalized?limit=10";
-//打印查看信息
-console.log(new Promise(function(resolve,reject){ }));
 
-//一个简单的实例
-function start() {
-    return new Promise(function(resolve, reject) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('get',url,true);
-    
+const p = new Promise((resolve,reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('get',url,true);
+
+    xhr.onreadystatechange = () => {
         if(xhr.readyState === 4){
-            if ((xhr.status>=200 && xhr.status<300) || xhr.status==304){
-                resolve('请求成功');
-            } else {
-                reject('请求失败');
+            if((xhr.status>=200 && xhr.status<300) || xhr.status==304){
+                resolve(xhr)
+            }else{
+                reject()
             }
         }
-        xhr.send();
-    });
-}
-
-start().then(function success(){
-    const result = JSON.parse(xhr.responseText)
-    const data = result.result;
-    const data_length = data.length;
-    var element = document.querySelector('.list');
-    console.log(result);
-    console.log(data);
-    for(var i=0; i<data_length; i++){
-        var song_list = document.createElement('li');
-        var list_image = document.createElement('img');
-        var list_name = document.createElement('div');
-        list_name.classList.add('.name');
-        list_image.setAttribute('src', data[i].picUrl);
-        song_list.appendChild(list_image);
-        list_name.innerHTML = data[i].name;
-        song_list.appendChild(list_name);
-        element.appendChild(song_list);
     }
-}).catch(function error(){
-    console.log('真的失败啦！')
-});
+    xhr.send();
+})
+p
+    .then((xhr) => {
+        const result = JSON.parse(xhr.responseText);
+        const data = result.result;
+        const data_length = data.length;
+        var element = document.querySelector('.list')
+        console.log(result);
+        console.log(data);
+        console.log('请求成功');
+        
+        for(var i=0; i<data_length; i++){
+            var song_list = document.createElement('li');
+            var list_image = document.createElement('img');
+            var list_name = document.createElement('div');
+            list_name.classList.add('.name')
+            list_image.setAttribute('src', data[i].picUrl);
+            song_list.appendChild(list_image)
+            list_name.innerHTML = data[i].name;
+            song_list.appendChild(list_name)
+            element.appendChild(song_list);
+        }
+    })
+    .catch(() => {
+        console.log('再试一试');
+    })
