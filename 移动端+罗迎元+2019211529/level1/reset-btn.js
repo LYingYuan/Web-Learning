9b44484cf4ca3f8fa85eef83c49f3cc003fb42db
LYY <luoyingyuan06@qq.com>
2021-12-19 15:24:30 +0800

    保存进度

---
+++ 移动端+罗迎元+2019211529/level1/reset-btn.js
@@ -0,0 +1,27 @@
+// 返回顶部
+var reset_btn = document.querySelector(".reset");
+reset_btn.style.display = "none";//先将按钮隐藏起来
+
+window.addEventListener("scroll", scrollHandler);
+function scrollHandler(e){
+    var distance_Y = document.documentElement.scrollTop || document.body.scrollTop;
+    if(distance_Y > 50){
+        reset_btn.style.display = "block";
+    }else{
+        reset_btn.style.display = "none";
+    }
+}
+
+//然后给reset_btn添加点击事件，用计时器interval来循环，步长为5，scrollTop依次减5，时间每50ms循环一次，直到scrollTop为0清除计时器
+reset_btn.addEventListener("click", clickHandler);
+function clickHandler(e){
+    let time = setInterval(()=>{
+        var distanceY = document.documentElement.scrollTop || document.body.scrollTop;
+            if (distanceY == 0){
+                clearInterval(time);
+                return;
+            } 
+            var speed = Math.ceil(distanceY/16);
+            document.documentElement.scrollTop=distanceY-speed;
+        },16)
+}
