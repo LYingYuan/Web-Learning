07b74d20d03f33ac975c7526fcab83dd1c2f0043
LYY <luoyingyuan06@qq.com>
2022-01-22 18:20:33 +0800

    吃个饭先

--- JS复习及补充/level4/script/script-search.js
+++ JS复习及补充/level4/script/script-search.js
@@ -1,6 +1,5 @@
 // 实现功能：1.清除添加记录
-//          2.点击城市跳转界面
-
+//          2.点击城市跳转界面【】可优化，时间不够
 
 // 1.清除添加记录 start
 const del_history_btn = document.querySelector(".del-city");
@@ -15,3 +14,21 @@ del_history_btn.onclick = function delCity() {
     }
 };
 // 1.清除添加记录 end
+
+// 2.点击城市跳转界面 start
+// 点击当前城市跳转
+const city_click_list_1 = document
+    .querySelector(".current-city")
+    .querySelector(".city-list")
+    .querySelectorAll("li");
+// 点击历史城市跳转
+const city_click_list_2 = document
+    .querySelector(".append-record")
+    .querySelector(".city-list")
+    .querySelectorAll("li");
+// 点击热搜城市跳转
+const city_click_list_3 = document
+    .querySelector(".hot-city")
+    .querySelector(".city-list")
+    .querySelectorAll("li");
+// 2.点击城市跳转界面 end
