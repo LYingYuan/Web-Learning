eaeb8c302e3a4b00ccc59b758acdd8f7d16ad9af
LYY_Mac <luoyingyuan06@qq.com>
2022-02-04 14:04:22 +0800

    first commit

---
+++ 7 JS复习及补充+罗迎元+2019211529/level1/script.js
@@ -0,0 +1,5 @@
+let str = "I'm?���going�??�to�?�take�??�a?�trip�?�to��?daocheng�?�on��May Day."
+
+let my_str = /^([[a-z]|[A-Z]|\'|\,|\.])/
+
+console.log(str) // 'I'm going to take a trip to Daocheng on May Day.'
