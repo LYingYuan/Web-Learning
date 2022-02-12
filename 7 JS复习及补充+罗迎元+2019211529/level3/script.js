b4e11b986151b5ed0ba0edcb8e69b544a59b295a
LYY <luoyingyuan06@qq.com>
2022-02-12 20:18:02 +0800

    7-level3完成

--- 7 JS复习及补充+罗迎元+2019211529/level3/script.js
+++ 7 JS复习及补充+罗迎元+2019211529/level3/script.js
@@ -1,15 +1,59 @@
 function Student(name) {
-    function People(name) {}
+    function People(name) {
+        // 任务队列
+        this.task = [];
+        this.task.push(() => {
+            console.log(`Hi! This is ${name}!`);
+            this.next();
+        });
+        setTimeout(() => {
+            this.next();
+        }, 0);
+        return this;
+    }
 
-    People.prototype.sleep = function (time) {};
+    People.prototype.next = function () {
+        const cb = this.task.shift();
+        // 先检查cb存不存在，如果不存在就不必执行cb()了，如果存在会执行该方法
+        cb && cb();
+    };
 
-    People.prototype.sleepFirst = function (time) {};
-    People.prototype.study = function (course) {};
+    People.prototype.sleep = function (time) {
+        this.task.push(() => {
+            setTimeout(() => {
+                console.log(`Wake up after ${time}`);
+                this.next();
+            }, time * 1000);
+        });
+        return this;
+    };
+
+    People.prototype.sleepFirst = function (time) {
+        // unshift()添加到数组首位
+        this.task.unshift(() => {
+            setTimeout(() => {
+                console.log(`Wake up after ${time}`);
+                this.next();
+            }, time * 1000);
+        });
+
+        return this;
+    };
+
+    People.prototype.study = function (course) {
+        this.task.push(() => {
+            setTimeout(() => {
+                console.log(`Study ${course}~`);
+                this.next();
+            }, 0);
+        });
+        return this;
+    };
 
     return new People(name);
 }
 
-// Student('fxy');
-// Student('fxy').sleep(3).study('javascript');
-// Student('fxy').study('javascript').study('Vue');
-// Student('fxy').sleepFirst(5).study('Ajax');
+// Student("fxy");
+// Student("fxy").sleep(3).study("javascript");
+// Student("fxy").study("javascript").study("Vue");
+Student("fxy").sleepFirst(5).study("Ajax");
