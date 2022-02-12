1928a48761b9b3233a0fd9c1ee36528d24673954
LYY <luoyingyuan06@qq.com>
2022-02-12 17:12:29 +0800

    7-level2完成

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
-let message;   // this variable is declared but has a value of undefined
-           
-// make sure this variable isn't declared
-// let age
-           
-console.log(message);  // "undefined"
-console.log(age);      // causes an error
-UndefinedExample03.js
+var name = "The Window";
+
+var object = {
+    name: "My Object",
+
+    getNameFunc: function () {
+        return function () {
+            return this.name;
+        };
+    },
+};
+
+alert(object.getNameFunc()());
