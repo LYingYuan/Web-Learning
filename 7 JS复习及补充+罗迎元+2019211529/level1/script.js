e0e73b8273d756384b456288eee08cf1abbe03f3
LYY <luoyingyuan06@qq.com>
2022-02-12 14:34:11 +0800

    7-level1完成

--- 7 JS复习及补充+罗迎元+2019211529/level1/script.js
+++ 7 JS复习及补充+罗迎元+2019211529/level1/script.js
@@ -1,5 +1,16 @@
-let str = "I'm?���going�??�to�?�take�??�a?�trip�?�to��?daocheng�?�on��May Day."
+let str = "I'm?���going�??�to�?�take�??�a?�trip�?�to��?daocheng�?�on��May Day.";
+let my_str1 = /(?<=[^a-z\'A-Z\.]+[?])([a-z])/g; //需要转换为大写的字母
+let my_str2 = /(?<=[^a-z\'A-Z\.]+[?])([a-z])/;
+let my_str3 = /([^a-z\'A-Z\.]+)/g; //替换为空格
 
-let my_str = /^([[a-z]|[A-Z]|\'|\,|\.])/
+// 将特殊符号后面问号跟着的字母转换为大写字母
+// 匹配
+let chars = str.match(my_str1);
+for (let i = 0, length = chars.length; i < length; i++) {
+    str = str.replace(my_str2, chars[i].toUpperCase());
+}
 
-console.log(str) // 'I'm going to take a trip to Daocheng on May Day.'
+// 替换为空格
+str = str.replace(my_str3, " ");
+
+console.log(str); // 'I'm going to take a trip to Daocheng on May Day.'
