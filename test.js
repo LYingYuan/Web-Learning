2930faf6e28d4c5953e353de01d8af02e4d3013d
LYY <luoyingyuan06@qq.com>
2022-02-18 13:48:57 +0800

    保存进度

--- test.js
+++ test.js
@@ -1,23 +1,43 @@
-var users = {};
-io.on("connection", function (socket) {
-    io.sockets.emit("connect", { con: "connected" });
-    socket.on("location", function (from, to, msg) {
-        if (to in users) {
-            //console.log('to'+to+'   '+msg);
-            users[to].emit("to" + to, msg);
+function cloneLoop(x) {
+    const root = {};
+
+    // 栈
+    const loopList = [
+        {
+            parent: root,
+            key: undefined,
+            data: x,
+        },
+    ];
+
+    while (loopList.length) {
+        // 深度优先
+        const node = loopList.pop();
+        const parent = node.parent;
+        const key = node.key;
+        const data = node.data;
+
+        // 初始化赋值目标，key为undefined则拷贝到父元素，否则拷贝到子元素
+        let res = parent;
+        if (typeof key !== "undefined") {
+            res = parent[key] = {};
         }
-    });
-    socket.on("newUser", function (user) {
-        if (user in users) {
-            socket.emit("exist", {});
-        } else {
-            users[user] = socket;
-            if (users["u2"]) {
-                io.sockets.emit("stateok", {});
+
+        for (let k in data) {
+            if (data.hasOwnProperty(k)) {
+                if (typeof data[k] === "object") {
+                    // 下一次循环
+                    loopList.push({
+                        parent: res,
+                        key: k,
+                        data: data[k],
+                    });
+                } else {
+                    res[k] = data[k];
+                }
             }
         }
-    });
-    socket.on(".disconnect-btn", function () {
-        socket.emit("disconnection");
-    });
-});
+    }
+
+    return root;
+}
