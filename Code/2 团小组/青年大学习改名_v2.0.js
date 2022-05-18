3a1021acba302b2a0c7b95b74faad839034faf5c
LYingYuan <1970027041@qq.com>
2022-05-18 21:07:37 +0800

    升级青年大学习改名脚本成功

---
+++ Code/2 团小组/青年大学习改名_v2.0.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+const data = "2022-05-12";
+
+// 文件目录
+// G:\OneDrive\Luo\工作\团小组\青年大学习
+const main_file_path = path.join(
+  "G:",
+  "OneDrive",
+  "Luo",
+  "工作",
+  "团小组",
+  "青年大学习"
+);
+
+const final_main_file_path = path.join(main_file_path, data);
+
+// 图片最终名称
+const final_names = [
+  "2019211529-罗迎元",
+  "2019211462-刘凯",
+  "2019214489-张蕴涵",
+  "2019211524-周渝",
+  "2019211523-王燚",
+];
+
+// 各小组成员名称（与 final_names 顺序对应）
+const file_names = ["罗迎元", "刘凯", "张蕴涵", "周渝", "王燚"];
+
+for (let i = 0; i < file_names.length; i++) {
+  const picture_old_path = path.join(main_file_path, file_names[i]);
+  fs.readdir(picture_old_path, (err, picture_file) => {
+    if (err) {
+      throw err;
+    }
+    const new_name = change_file_name(picture_file[0], final_names[i]);
+    fs.rename(
+      path.join(picture_old_path, picture_file[0]),
+      path.join(final_main_file_path, new_name),
+      (err) => {
+        if (err) {
+          throw err;
+        }
+      }
+    );
+  });
+}
+
+// 获得全新名称
+function change_file_name(old_name, new_name) {
+  // 切割文件名(以最后一个点为分隔符)
+  const cut_index = old_name.lastIndexOf(".");
+  // 保存文件后缀名
+  const type_name = old_name.slice(cut_index);
+  // 组合成新名称并返回
+  return new_name + type_name;
+}
